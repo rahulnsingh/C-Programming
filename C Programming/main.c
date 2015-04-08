1b510d4d35fff53664eb140ec72d12940bc72a96
Rahul <Rahul@Rahuls-MacBook-Pro.local>
2015-04-09 00:01:52 +0200

    Initial Commit

---
+++ C Programming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C Programming
+//
+//  Created by Rahul on 09/04/15.
+//  Copyright (c) 2015 Rahul. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
